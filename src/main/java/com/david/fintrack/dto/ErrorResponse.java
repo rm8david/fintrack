a8aecdc656b722b9cef8a311ef14d9783825c8fd
David Rodriguez Martin <rm8david@gmail.com>
2025-07-16 13:40:32 +0200

    Update ErrorResponse.java

--- src/main/java/com/david/fintrack/dto/ErrorResponse.java
+++ src/main/java/com/david/fintrack/dto/ErrorResponse.java
@@ -1,6 +1,8 @@
 package com.david.fintrack.dto;
 
 public class ErrorResponse {
+
+    // clase Error personalizada para manejar errores
     private String message;
 
     public ErrorResponse(String message) {
